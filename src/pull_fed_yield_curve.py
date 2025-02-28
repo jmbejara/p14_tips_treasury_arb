d1f45a17822d52bd441f6476dc8bfd339047be6a
BaileyMeche <baileymeche@uchicago.edu>
2025-02-28 00:53:29 -0600

    Organized files for Nominal Treasury Yields

--- src/pull_fed_yield_curve.py
+++ src/pull_fed_yield_curve.py
@@ -1,19 +1,10 @@
-"""
-This module contains functions to load the zero coupon yield curve from the Federal Reserve.
-It saves the pulled raw data to a parquet file for future use.
-Functions to load the raw/clean data from the parquet file are also provided for future use.
-
-"""
-
 import pandas as pd
 import requests
 from io import BytesIO
 from pathlib import Path
-import settings
 
-DATA_DIR = settings.DATA_DIR
-START_DATE = settings.START_DATE
-END_DATE = settings.END_DATE
+from settings import config
+DATA_DIR = config('DATA_DIR')
 
 
 def pull_fed_yield_curve():
@@ -26,18 +17,29 @@ def pull_fed_yield_curve():
     url = "https://www.federalreserve.gov/data/yield-curve-tables/feds200628.csv"
     response = requests.get(url)
     pdf_stream = BytesIO(response.content)
-    df = pd.read_csv(pdf_stream, skiprows=9, index_col=0, parse_dates=True)
-    cols = ['SVENY' + str(i).zfill(2) for i in range(1, 31)]
-    return df[cols]
+    df_all = pd.read_csv(pdf_stream, skiprows=9, index_col=0, parse_dates=True)
 
-def load_fed_yield_curve(data_dir=DATA_DIR):
-    path = data_dir  / "fed_yield_curve.parquet"
+    cols = ['SVENY' + str(i).zfill(2) for i in range(1, 31)]
+    df = df_all[cols]
+    return df_all, df
+
+def load_fed_yield_curve_all(data_dir=DATA_DIR):
+    path = data_dir / "fed_yield_curve_all.parquet"
     _df = pd.read_parquet(path)
     return _df
+
+def load_fed_yield_curve(data_dir=DATA_DIR):
+    path = data_dir / "fed_yield_curve.parquet"
+    _df = pd.read_parquet(path)
+    return _df
+
+def _demo():
+    _df = pull_fed_yield_curve(data_dir=DATA_DIR)
     
+
 if __name__ == "__main__":
-    df = pull_fed_yield_curve()
+    df_all, df = pull_fed_yield_curve()
+    path = Path(DATA_DIR) / "fed_yield_curve_all.parquet"
+    df_all.to_parquet(path)
     path = Path(DATA_DIR) / "fed_yield_curve.parquet"
     df.to_parquet(path)
-    
-    
